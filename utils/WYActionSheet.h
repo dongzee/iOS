c073a8172e1c71d3c4f5d1418c6bc4c431da8612
dongzee <260180966@qq.com>
2014-03-28 11:24:46 +0800

    actionsheet & alert comments

--- utils/WYActionSheet.h
+++ utils/WYActionSheet.h
@@ -4,46 +4,74 @@
 //
 //  Created by wanglidong on 13-5-2.
 //  Copyright (c) 2013年 wy. All rights reserved.
-//
+//  en custom actionsheet with block
+//  cn 自定义actionsheet控件, 使用block方式
 
-/////// simple DEMO ///////
 /*
+ * iOS4.0 or later, for GCD.
+ * Usage:
+ *
+ * ========================================
+     WYActionSheet *sheet = [[WYActionSheet alloc]initWithTitle:[NSString stringWithFormat:@"%d",[indexPath row]] cancelButtonTitle:@"CANCEL" cancelBlock:^(NSInteger buttonIndex){
+     
+     NSLog(@"%s [%d]",__func__,buttonIndex);
+     
+     } otherButtonTitles:@"1",^(NSInteger buttonIndex){
+     
+     NSLog(@"%s [%d]",__func__,buttonIndex);
+     
+     }, @"2",^(NSInteger buttonIndex){
+     
+     NSLog(@"%s [%d]",__func__,buttonIndex);
+     
+     },nil];
  
- WYActionSheet *sheet = [[[WYActionSheet alloc]initWithTitle:[NSString stringWithFormat:@"%d",[indexPath row]] cancelButtonTitle:@"CANCEL" cancelBlock:^(NSInteger buttonIndex){
- NSLog(@"%s [%d]",__func__,buttonIndex);
- } otherButtonTitles:@"1",^(NSInteger buttonIndex){
- NSLog(@"%s [%d]",__func__,buttonIndex);
- }, @"2",^(NSInteger buttonIndex){
- NSLog(@"%s [%d]",__func__,buttonIndex);
- },nil] autorelease];
+    // sets destructive(red) button. 
+    // -1 means not set. default is -1. 
+    // ignored when only one button
+    [sheet setDestructiveButtonIndex:0];
  
- [sheet setDestructiveButtonIndex:0];// sets destructive (red) button. -1 means none set. default is -1. ignored if only one button
- 
- [sheet showFromTabBar:self.tabBarController.tabBar];
- 
+    [sheet showFromTabBar:self.tabBarController.tabBar];
+ * ========================================
  */
 
 #import <UIKit/UIKit.h>
 typedef void (^WYActionSheetClickedButtonAtIndexBlock)(NSInteger buttonIndex); //
 
-@interface WYActionSheet : UIActionSheet<UIActionSheetDelegate>
+NS_CLASS_AVAILABLE_IOS(4_0) @interface WYActionSheet : UIActionSheet<UIActionSheetDelegate>
 
-@property(nonatomic, assign)BOOL shouldDismissWhenAppEnterBackground; // app回到后台时是否自动取消, Default is YES.
-@property(nonatomic, assign)BOOL shouldDismissWhenDeviceRotate; // 屏幕旋转时是否自动取消, Default is YES.
+// dismiss automatically when app enter backgroud
+// app回到后台时是否自动取消
+@property(nonatomic, assign)BOOL shouldDismissWhenAppEnterBackground; // Default is YES.
 
-/** 产生一个ActionSheet, 未使用ARC时需要手动释放
+// dismiss automatically when device rotate
+// 屏幕旋转时是否自动取消
+@property(nonatomic, assign)BOOL shouldDismissWhenDeviceRotate; // Default is YES.
+
+/**
+-- english comment
+ * new an actionsheet, not autorelease
+ * @param title             title
+ * @param cancelButtonTitle cancel button text
+ * @param cancelBlock       cancel block
+ * @param otherButtonTitles pairs of button & block, until nil. block must not nil.
+
+-- 中文注释
+ * 产生一个ActionSheet, 不是autorelease的, 故未使用ARC时需要手动释放
  *
- * @param title     提示信息
+ * @param title             提示信息
  * @param cancelButtonTitle 取消按钮
- * @param cancelBlock 取消处理
+ * @param cancelBlock       取消处理
  * @param otherButtonTitles 第一个nil之前参数有效, buttons和blocks必须【成对】出现, 且block不能为nil.
- *
- * @return 返回ActionSheet对象
  */
 - (id)initWithTitle:(NSString *)title cancelButtonTitle:(NSString *)cancelButtonTitle cancelBlock:(WYActionSheetClickedButtonAtIndexBlock)cancelBlock otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
 
-/** 显示一个ActionSheet, buttons和blocks必须成对出现
- *  同上
+/**
+-- english comment
+ * show an actionsheet, autorelease
+
+-- 中文注释
+ * 显示一个ActionSheet, autorelease的
  */
 + (void)showInview:(UIView *)view withTitle:(NSString *)title cancelButtonTitle:(NSString *)cancelButtonTitle cancelBlock:(WYActionSheetClickedButtonAtIndexBlock)cancelBlock otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
 
@@ -55,8 +83,12 @@ typedef void (^WYActionSheetClickedButtonAtIndexBlock)(NSInteger buttonIndex); /
 
 + (void)showFromRect:(CGRect)rect inView:(UIView *)view animated:(BOOL)animated withTitle:(NSString *)title cancelButtonTitle:(NSString *)cancelButtonTitle cancelBlock:(WYActionSheetClickedButtonAtIndexBlock)cancelBlock otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
 
-/** 显示一个ActionSheet, 有destructiveButton的, destructiveButtonTitle & destructiveBlock 不能为nil.
- *  同上
+/**
+-- english comment
+ * show an actionsheet has destructiveButton, autorelease. destructiveButtonTitle & destructiveBlock not nil.
+ 
+-- 中文注释
+ * 显示一个ActionSheet, 有destructiveButton, autorelease. destructiveButtonTitle & destructiveBlock 不能为nil.
  */
 + (void)showInview:(UIView *)view
          withTitle:(NSString *)title
--- utils/WYAlertView.h
+++ utils/WYAlertView.h
@@ -5,63 +5,88 @@
 //  Created by wanglidong on 13-4-25.
 //  Copyright (c) 2013年 wy. All rights reserved.
 //
-//  UIAlertView子类, 用于显示对话框。
+//  en custom AlertView with block
+//  cn 自定义AlertView控件, 使用block方式
 
-
-/////// simple DEMO ///////
 /*
- 
- [WYAlertView show:@"demo" message:@"this is a demo." cancelButtonTitle:@"cancel" cancelBlock:nil otherButtonTitles:
- @"button1",^{
- NSLog(@"click button1");
- },
- @"button2",^(NSInteger buttonIndex){
- NSLog(@"click button[%d]",buttonIndex); // 如果需要使用点击了哪个按钮
- },
- nil];
- 
+ * iOS4.0 or later, for GCD.
+ * Usage:
+ *
+ * ========================================
+     [WYAlertView show:@"demo" message:@"this is a demo." cancelButtonTitle:@"cancel" cancelBlock:nil otherButtonTitles:
+     @"button1",^{
+     
+        NSLog(@"click button1");
+     },
+     @"button2",^(NSInteger buttonIndex){
+        
+        NSLog(@"click button[%d]",buttonIndex);
+
+     }, nil];
+ * ========================================
  */
 
 #import <UIKit/UIKit.h>
 
 typedef void (^WYAlertViewClickedButtonAtIndexBlock)(NSInteger buttonIndex); //
 
-@interface WYAlertView : UIAlertView
+NS_CLASS_AVAILABLE_IOS(4_0) @interface WYAlertView : UIAlertView
 
-@property(nonatomic, assign)BOOL shouldDismissWhenAppEnterBackground; // app回到后台时是否自动取消, Default is YES.
+// dismiss automatically when app enter backgroud
+// app回到后台时是否自动取消
+@property(nonatomic, assign)BOOL shouldDismissWhenAppEnterBackground; // Default is YES.
 
-/** 产生一个Alert对话框,未使用ARC时需要手动释放
- *
- * @param title     弹出标题
- * @param message   提示信息
+/**
+-- english comment
+ * new an alert, not autorelease
+ * @param title              title
+ * @param message            message
+ * @param cancelButtonTitle  cancel button text
+ * @param cancelBlock button cancel block
+ * @param otherButtonTitles  pairs of button & block, until nil. block must not nil.
+ 
+-- 中文注释
+ * 创建一个对话框, 不是autorelease的
+ * @param title             弹出标题
+ * @param message           提示信息
  * @param cancelButtonTitle 取消按钮
- * @param cancelBlock 取消处理
+ * @param cancelBlock       取消处理
  * @param otherButtonTitles 第一个nil之前参数有效, buttons和blocks必须【成对】出现, 且block不能为nil.
- *
- * @return 返回对话框对象
  */
 - (id)initWithTitle:(NSString *)title message:(NSString *)message cancelButtonTitle:(NSString *)cancelButtonTitle cancelBlock:(WYAlertViewClickedButtonAtIndexBlock)cancelBlock otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
 
-/** 弹出一个对话框，buttons和blocks必须成对出现
- *
- * 同上
+/**
+-- english comment
+ * show an alert, autorelease
+
+-- 中文注释
+ * 弹出一个对话框, autorelease的
  */
 + (void)show:(NSString *)title message:(NSString *)message cancelButtonTitle:(NSString *)cancelButtonTitle cancelBlock:(WYAlertViewClickedButtonAtIndexBlock)cancelBlock otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
 
-/** 产生一个Alert对话框,未使用ARC时需要手动释放, 只有一个按钮
- *
- * @param title     弹出标题
- * @param message   提示信息
+/**
+-- english comment
+ * new an alert, only one button, not autorelease
+ * @param title       title
+ * @param message     message
+ * @param buttonTitle button text
+ * @param completion  button event
+
+-- 中文注释
+ * 创建一个对话框, 只有一个按钮, 不是autorelease的
+ * @param title       弹出标题
+ * @param message     提示信息
  * @param buttonTitle 按钮
- * @param completion 处理
- *
- * @return 返回对话框对象
+ * @param completion  处理
  */
 - (id)initWithTitle:(NSString *)title message:(NSString *)message buttonTitle:(NSString *)buttonTitle completion:(WYAlertViewClickedButtonAtIndexBlock)completion;
 
-/** 弹出一个对话框, 只有一个按钮
- *
- * 同上
+/**
+-- english comment
+ * show an alert, only one button
+ 
+-- 中文注释
+ * 弹出一个对话框, 只有一个按钮
  */
 + (void)show:(NSString *)title message:(NSString *)message buttonTitle:(NSString *)buttonTitle completion:(WYAlertViewClickedButtonAtIndexBlock)completion;
 
